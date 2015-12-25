ebd06f1c5b26e8b1f29b0fc273af68de8b5f0414
radu-matei <radu.matei@equilobe.com>
2015-12-25 12:59:57 +0200

    DB-26 Added authentication for application - added authorization filter for entire application - allowed annonymous requests for the home controller

--- HomeServices/App_Start/FilterConfig.cs
+++ HomeServices/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace HomeServices
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
